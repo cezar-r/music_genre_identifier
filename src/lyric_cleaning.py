2f4c410d6fb0193b46370adefe445dbc899e5622
Cezar <cez.rata1@gmail.com>
2021-07-10 20:12:47 -0700

    updated cleaning

--- src/lyric_cleaning.py
+++ src/lyric_cleaning.py
@@ -1,5 +1,5 @@
 def clean(string):
-	bad_chars = ['</i>', '<i>', '[', ']', '(', ')', 'INTRO:', 'Chorus:', 'CHORUS:', 'Verse:', 'Bridge:', 'Hook:', 'HOOK:', '—']
+	bad_chars = ['</i>', '<i>', '[', ']', '(', ')', 'INTRO:', 'Chorus:', 'CHORUS:', 'Verse:', 'Bridge:', 'Hook:', 'HOOK:', '—', '–', '�']
 	for char in bad_chars:
 		string = string.replace(char, "")
 	return string
