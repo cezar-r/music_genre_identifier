deb3ead3a3cd3e4e5e8d96e0979380b2c53c6e0c
cezar-r <59450965+cezar-r@users.noreply.github.com>
2021-07-16 20:15:35 -0700

    Update gui_display.py

--- src/gui_display.py
+++ src/gui_display.py
@@ -21,30 +21,37 @@ class GUI:
     	artist_name: str
     		string of artist name
     """
-	layout = [[sg.Text('Enter song name', 
+	layout = [
+		[sg.Text('Enter song name', 
 				font=('Segoe UI Semibold', 20)), 
-				sg.Input(background_color = 'black', 
+			sg.Input(background_color = 'black', 
 				text_color = 'white', 
 				key = 'i1', 
-				font=('Segoe UI Semibold', 20))],
+				font=('Segoe UI Semibold', 20))
+		],
 		[sg.Text('Enter artist name', 
 				font=('Segoe UI Semibold', 20)), 
 			sg.Input(background_color = 'black', 
 				text_color = 'white', 
 				key = 'i2', 
-				font=('Segoe UI Semibold', 20))],
+				font=('Segoe UI Semibold', 20))
+		],
 		[sg.Button('Run', 
 				button_color = 'fuchsia', 
 				size=(15, 0)), 
 			sg.Button('Clear', 
 				button_color = 'white', 
-				size=(15, 0))],
+				size=(15, 0))
+		],
 		[sg.Text('Prediction:', 
-				font=('Segoe UI Semibold', 20))],
+				font=('Segoe UI Semibold', 20))]
+		,
 		[sg.Text('              ', 
 				font=('Segoe UI Semibold', 20), 
 				key = 'pred', 
-				size=(25,1))]]
+				size=(25,1))
+		]
+	]
 
 	window = sg.Window('A.I. Genre Classifier', layout, size=(500, 300))
 
